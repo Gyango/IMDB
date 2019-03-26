7b5a775b19f684f6532880724030eaac609e6d74
Srdjan Palanacki <srdjanpalanacki88@gmail.com>
2019-03-26 23:48:47 +0100

    Initial commit
    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+
+export function App() {
+  return (
+    <div>
+      <h1>IMDB</h1>
+      This is a sample stateful and server-side rendered React IMDB application.
+    </div>
+  );
+}
