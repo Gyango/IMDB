53ef9e257d1ccdb57f8077ea2cbc9203e89b8cef
Srdjan Palanacki <srdjanpalanacki88@gmail.com>
2019-04-04 00:02:55 +0200

    Added navigation bar

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,27 @@ export function App() {
   return (
     <div>
       <h1>IMDB</h1>
+      <NavigationBar/>
       This is a sample stateful and server-side rendered React IMDB application.
     </div>
   );
+}
+
+function NavigationBar(){
+
+
+    const [movieType, setType] = useState("Popular");
+
+    return(
+      <ul>
+        <li class="dropdown">
+          <a href="javascript:void(0)" class="dropbtn">{movieType}</a>
+          <div class="dropdown-content">
+            <a onClick={() => setType("Popular")}>Popular</a>
+            <a onClick={() => setType("Upcoming")}>Upcoming</a>
+          </div>
+        </li>
+      </ul>
+    )
+
 }
