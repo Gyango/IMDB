e6ad76ad8fd975f2ff1af8bc6be4d7694990251c
Srdjan Palanacki <srdjanpalanacki88@gmail.com>
2019-05-26 19:02:00 +0200

    Implemented request for popular and upcoming movies

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,8 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import Dropdown from 'react-dropdown'
 
-const url = 'https://cors-anywhere.herokuapp.com/http://api.themoviedb.org/3/discover/movie?api_key=a787ed25d3a7aef96d3079f0269df80b&primary_release_date.gte=1999-09-15&primary_release_date.lte=2014-10-22';
+const urlForPopular = 'https://cors-anywhere.herokuapp.com/http://api.themoviedb.org/3/movie/popular?api_key=a787ed25d3a7aef96d3079f0269df80b&region=en-US';
+const urlForUpcoming = 'https://cors-anywhere.herokuapp.com/http://api.themoviedb.org/3/movie/upcoming?api_key=a787ed25d3a7aef96d3079f0269df80b&region=US';
 
 const MovieList = (props) => (
   <div>
@@ -50,27 +51,27 @@ export class App extends React.Component {
   }
 }
 
-
 class NavigationBar extends React.Component{
 
+  async newHandleSubit(url, type){
+    console.log("Sync function param: " + url + " " +type);
+    this.setState({movieType: type})
+    const resp = await axios.get(url);
+    console.log(resp.data);
+    this.props.onSubmit(resp.data);
+  };
+
     state = { movieType: 'Popular'};
-
-    handleSubmit = async (event) => {
-      	event.preventDefault();
-        const resp = await axios.get(url);
-        this.props.onSubmit(resp.data);
-      };
-
 
 render(){
     return(
       <ul>
         <li className="dropdown">
-        <form onClick={this.handleSubmit}>
+        <form>
           <a href="javascript:void(0)" className="dropbtn">{this.state.movieType}</a>
           <div className="dropdown-content">
-            <a onClick={() => this.setState({movieType: 'Popular'})}>Popular</a>
-            <a onClick={() => this.setState({movieType: 'Upcoming'})}>Upcoming</a>
+            <a onClick={() => this.newHandleSubit(urlForPopular, 'Popular')}>Popular</a>
+            <a onClick={() => this.newHandleSubit(urlForUpcoming, 'Upcoming')}>Upcoming</a>
           </div>
           </form>
         </li>
